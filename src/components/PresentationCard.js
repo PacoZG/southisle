16957343d89d730eaa3acb7a82c1b6f759ed1231
Francisco Zavala <pako.zavala@gmail.com>
2021-09-10 12:25:12 +0300

    Update 10.09.2021

--- src/components/PresentationCard.js
+++ src/components/PresentationCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import lotta from "../assets/Charlotta.jpg";
+import { EmailIcon, LinkedinIcon, WhatsappIcon } from "react-share";
 
 const PresentationCard = () => {
   return (
@@ -54,8 +55,6 @@ const PresentationCard = () => {
             <li className="li">Usability of workspace</li>
             <li className="li">Strategic Facilities Management</li>
           </ul>
-        </div>
-        <div className="column2">
           <h1 className="h1-2">Community Envolvement</h1>
           <h3 className="h3" style={{ fontSize: "16px" }}>
             MLL Ruoholahti-Jätkäsaari local organisation
@@ -72,7 +71,78 @@ const PresentationCard = () => {
               help facilitated by the main organisation
             </li>
           </ul>
+        </div>
+        <div className="column2">
           <h1 className="h1-2">Work experience</h1>
+          <h4 className="h4" style={{ padding: "11px" }}>
+            In addition to day-to-day tasks involving the core business of
+            restaurant services, such as customer service, quality management
+            and ﬂoor manager duties, my responsibilities were also as following:
+          </h4>
+          <h3 className="h3" style={{ fontSize: "16px" }}>
+            Shift Manager, Makkis ja Pekkis Oy
+          </h3>
+          <h4 className="h4">20.1.2020 - On going (layoff)</h4>
+          <h4 className="h4">Responsabilities:</h4>
+          <ul className="ul-work">
+            <li className="li">Opening of ﬁrst restaurant</li>
+            <li className="li">Developing concept</li>
+            <li className="li">Reporting</li>
+            <li className="li">
+              Cooperation with Lounastaja to enhance user experience
+            </li>
+          </ul>
+          <h3 className="h3" style={{ fontSize: "16px" }}>
+            Head Waiter, Kämp Oy
+          </h3>
+          <h4 className="h4">1.7.2016-31.1.2020</h4>
+          <h4 className="h4">Responsabilities:</h4>
+          <ul className="ul-work">
+            <li className="li">LHW standards</li>
+            <li className="li">Development of service process</li>
+            <li className="li">Training, Introduction material and -process</li>
+            <li className="li">Working closely with 3rd party suppliers</li>
+          </ul>
+          <h3 className="h3" style={{ fontSize: "16px" }}>
+            Restaurant Manager, EF-Restaurants
+          </h3>
+          <h4 className="h4">20.1.2020 - On going (layoff)</h4>
+          <h4 className="h4">Responsabilities:</h4>
+          <ul className="ul-work">
+            <li className="li">Opening and consulting on ﬁrst restaurant</li>
+            <li className="li">Work schedules and creating follow up</li>
+            <li className="li">Inventory Management</li>
+          </ul>
+          <h3 className="h3" style={{ fontSize: "16px" }}>
+            Head Waiter/Event Coordinator Palace <br /> Kämp Ravintolat Oy/Royal
+            Ravintolat Oy
+          </h3>
+          <h4 className="h4">1.5.2009-14.4.2015</h4>
+          <h4 className="h4">Responsabilities:</h4>
+          <ul className="ul-work">
+            <li className="li">Meeting and Event Management</li>
+            <li className="li">Cash ﬂow and Reporting</li>
+            <li className="li">Inventory Management</li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer-alignment">
+        <div className="footer">
+          <a
+            href="https://www.linkedin.com/in/charlottasoderholm/"
+            target="blank"
+          >
+            <LinkedinIcon size={40} borderRadius={10} />
+          </a>
+          <a
+            href="https://wa.me/358405318180?text=I%20will%20like%20to%20contact%20you%20to%20arrange%20an%20interview"
+            target="blank"
+          >
+            <WhatsappIcon size={40} borderRadius={10} />
+          </a>
+          <a href="mailto:charlotta.soderholm@pp.inet.fi" target="blank">
+            <EmailIcon size={40} borderRadius={10} />
+          </a>
         </div>
       </div>
     </div>
